89fe15f5ea528c0965b113cb17abc45c600c40dc
lotta <allanlotta.dev@gmail.com>
2024-06-14 21:09:05 -0300

    feat: change layout

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,26 +5,27 @@ import heart from "../assets/images/cards/heart.svg";
 import spade from "../assets/images/cards/spade.svg";
 import diamond from "../assets/images/cards/diamond.svg";
 import club from "../assets/images/cards/club.svg";
+import cardHidden from "../assets/images/cards/back.png";
+
 export default function Card({
   card,
-  playerCardVisible = false,
-  isMyCard = false,
+  type = "community",
 }: {
   card: { suit: string; rank: string };
-  playerCardVisible?: boolean;
-  isMyCard?: boolean;
+  type: "community" | "playerVisible" | "playerHidden" | "my";
 }) {
   const { suit, rank } = card;
   // width of the card
   const defaultWidth = 30;
   const playerCardVisibleWidth = 40;
+  const playerCardHiddenWidth = 30;
   const myCardWidth = 50;
   // size of the rank
   const defaultRankSize = 16;
   const myCardRankSize = 22;
   const playerCardRankSize = 18;
 
-  const rankSize = playerCardVisible ? playerCardRankSize : isMyCard ? myCardRankSize : defaultRankSize;
+  const rankSize = type === "playerVisible" ? playerCardRankSize : type === "my" ? myCardRankSize : defaultRankSize;
 
   const suitIcon =
     suit === "hearts"
@@ -34,11 +35,17 @@ export default function Card({
       : suit === "diamonds"
       ? diamond
       : club;
-  const width = playerCardVisible ? playerCardVisibleWidth : isMyCard ? myCardWidth : defaultWidth;
-  const height = !isMyCard ? width * 1.3 : width * 1.2;
+  const width = type === "playerVisible" ? playerCardVisibleWidth : type === "my" ? myCardWidth : type === "playerHidden" ? playerCardHiddenWidth : defaultWidth;
+  const height = type !== "my" ? width * 1.3 : width * 1.2;
   const rankColor = suit === "hearts" || suit === "diamonds" ? "red" : "black";
 
-  const styles = dynamicStyles(width, height, rankColor, rankSize, isMyCard);
+  const styles = dynamicStyles(width, height, rankColor, rankSize, type === "my");
+
+  if (type === "playerHidden") {
+    return (
+      <Image contentFit="contain" source={cardHidden} style={styles.playerCardHidden} />
+    );
+  }
 
   return (
     <View style={styles.container}>
@@ -60,12 +67,17 @@ const dynamicStyles = (width: number, height: number, rankColor: string, rankSiz
       borderRadius: 5,
       backgroundColor: "#e3e3e3",
       borderWidth: 1,
-      borderColor: !isMyCard ? "rgba(0, 0, 0, 0.1)" : "yellow",
+      borderColor: "rgba(0, 0, 0, 0.1)",
       elevation: 1,
     },
     image: {
       height: width * 0.5,
       width: width * 0.5,
+    },
+    playerCardHidden: {
+      width,
+      height,
+      borderRadius: 5,
     },
     rank: {
       position: "absolute",
